5979eed9dde70f79286219da49f93d11ead81f87
Lela810 <lklaus@online.gibz.ch>
2022-08-11 17:48:25 +0200

    update

--- js/whatsapp/whatsappGroup.js
+++ js/whatsapp/whatsappGroup.js
@@ -1,4 +1,5 @@
 const { loadAllActivities, createActivity } = require('../db.js')
+const { List } = require('whatsapp-web.js');
 
 
 async function whatsappGroup(userMenu, message) {
@@ -7,13 +8,13 @@ async function whatsappGroup(userMenu, message) {
 
     let messageText = message.body.replace('@41794706505 ', '')
     switch (messageText) {
-        case 1:
+        case '1':
             userMenu = 1;
             break;
-        case 2:
+        case '2':
             userMenu = 2;
             break;
-        case 3:
+        case '3':
             userMenu = 3;
             break;
     }
@@ -25,15 +26,19 @@ async function whatsappGroup(userMenu, message) {
     }
 
     if (userMenu == 1) {
-        await chat.sendMessage(await loadAllActivities())
+        console.log(await loadAllActivities())
+        await chat.sendMessage("test")
+        return (1)
     }
 
     if (userMenu == 2) {
         await chat.sendMessage(await loadAllActivities())
+        return (2)
     }
 
     if (userMenu == 3) {
-        createActivity()
+        //await createActivity()
+        return (3)
             //await chat.sendMessage(await loadAllActivities())
     }
 
